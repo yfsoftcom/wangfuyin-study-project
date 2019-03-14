8f72a22c97c1e632841a8828a8de0e67adda493e
yfsoftcom <yfsoftcom@126.com>
2019-03-14 14:52:21 +0800

    change demo.yunplus.io

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 # bin/bash
 npm run build
 mv build foo
-scp -r ./foo root@a.yunplus.io:/home/yf/nodejs/projects/demo.yunplus.io/
-scp -r ./foo/static root@a.yunplus.io:/home/yf/nodejs/projects/demo.yunplus.io/
+scp -r ./foo root@a.yunplus.io:/home/yf/projects/docker/server-yunplus-io-docker-compose/nginx/www/
+scp -r ./foo/static root@a.yunplus.io:/home/yf/projects/docker/server-yunplus-io-docker-compose/nginx/www/
